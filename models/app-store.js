ef4eec32f13c41bbfe0f368c2822eab5a6f5006d
Michael Louis Langan <20106749@mail.wit.ie>
2026-02-11 16:12:13 +0000

    Update app-store.js

--- models/app-store.js
+++ models/app-store.js
@@ -1,9 +1,18 @@
 'use strict';
 
-import logger from "../utils/logger";
+import logger from '../utils/logger.js';
+import JsonStore from './json-store.js';
 
 const appStore = {
-    // function to manage JSON data to be added
-}
+
+  store: new JsonStore('./models/app-store.json', { info: {} }),
+  collection: 'info',
+  array: 'creators',
+
+  getAppInfo() {
+    return this.store.findAll(this.collection);
+  },
+
+};
 
 export default appStore;
